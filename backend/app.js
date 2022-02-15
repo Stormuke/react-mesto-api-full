870410e56c559f40b44f47d35a1b15626d717bea
Stormuke <fen1x@yandex.ru>
2022-02-15 22:46:57 +0300

    fix: cors

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,9 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(cors);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(requestLogger);
 app.post('/signup', userValidation, createUser);
